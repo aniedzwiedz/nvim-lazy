e1e3aa497ed220be4c36faf495627d30bad929a2
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2023-10-26 23:43:01 +0200

    gitlinker

--- lua/plugins/enabled.lua
+++ lua/plugins/enabled.lua
@@ -123,4 +123,28 @@ return {
     },
   },
   { "b0o/schemastore.nvim" },
+  {
+    "ruifm/gitlinker.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitlinker").setup({
+        opts = {
+          -- remote = 'github', -- force the use of a specific remote
+          -- adds current line nr in the url for normal mode
+          add_current_line_on_normal_mode = true,
+          -- callback for what to do with the url
+          -- action_callback = require("gitlinker.actions").open_in_browser,
+          action_callback = require("gitlinker.actions").copy_to_clipboard,
+          -- print the url after performing the action
+          print_url = true,
+          -- mapping to call url generation
+          mappings = "<leader>gy",
+        },
+        callbacks = {
+          ["git.gtech.com"] = require("gitlinker.hosts").get_bitbucket_type_url,
+        },
+      })
+    end,
+    requires = "nvim-lua/plenary.nvim",
+  },
 }
