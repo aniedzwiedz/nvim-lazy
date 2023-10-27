4a7dfe337615b7b2c45f2dea22f6b27e05a6c2a3
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2023-10-27 16:07:40 +0200

    lspsaga hover_doc

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
+    init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- change a keymap
+    keys[#keys + 1] = { "K", "<cmd>Lspsaga hover_doc<cr>" }
+    -- disable a keymap
+    -- keys[#keys + 1] = { "K", false }
+    -- add a keymap
+    -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+  end,
+
   opts = {
     servers = {
       marksman = {},
