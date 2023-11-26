8d13dc3c5d114dfba2043699057438b7158224c0
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2023-11-26 21:59:54 +0100

    asdf

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -3,3 +3,31 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = true,
 }
+
+-- local M = {
+--   "NvChad/nvim-colorizer.lua",
+--   event = { "BufReadPost", "BufNewFile" },
+-- }
+--
+-- function M.config()
+--   require("colorizer").setup {
+--     filetypes = {
+--       "typescript",
+--       "typescriptreact",
+--       "javascript",
+--       "javascriptreact",
+--       "css",
+--       "html",
+--       "astro",
+--       "lua",
+--     },
+--     user_default_options = {
+--       names = false,
+--       rgb_fn = true,
+--       tailwind = "both",
+--     },
+--     buftypes = {},
+--   }
+-- end
+--
+-- return M
