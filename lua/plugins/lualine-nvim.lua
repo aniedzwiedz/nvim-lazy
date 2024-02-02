19fe0396e66a7001286b3a346b29bc860f8e2095
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2024-02-02 17:27:01 +0100

    lualine

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -24,6 +24,7 @@ return {
     local icons = require("lazyvim.config").icons
     local Util = require("lazyvim.util")
     local linters = require("lint").get_running()
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
     vim.o.laststatus = vim.g.lualine_laststatus
 
@@ -49,17 +50,27 @@ return {
         lualine_c = {
           Util.lualine.root_dir(),
           {
-            "diagnostics",
+            "diff",
             symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
             },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
           },
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { Util.lualine.pretty_path() },
         },
+
         lualine_x = {
           -- stylua: ignore
           {
@@ -85,22 +96,13 @@ return {
             color = Util.ui.fg("Special"),
           },
           {
-            "diff",
+            "diagnostics",
             symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
             },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
           },
         },
 
@@ -111,6 +113,13 @@ return {
 
         lualine_z = {
 
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
           -- function()
           --   return " " .. os.date("%R")
           -- end,
