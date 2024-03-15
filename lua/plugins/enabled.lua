edb735294e2ce0b0c13a6f1e2204bce853ef5cd2
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2024-03-15 20:55:09 +0100

    feat(neogit): Adding negogit

--- lua/plugins/enabled.lua
+++ lua/plugins/enabled.lua
@@ -170,6 +170,21 @@ return {
     --   },
     -- },
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+    opts = {
+      kind = "split",
+    },
+  },
   { "b0o/SchemaStore.nvim" },
   { "folke/neodev.nvim" },
   -- {
