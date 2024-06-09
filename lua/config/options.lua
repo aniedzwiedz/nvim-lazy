b6d52249837244ffad78f258f1640b37dd957dbb
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2024-06-09 14:35:59 +0200

    neogit + update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- This file is automatically loaded by plugins.core
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Enable LazyVim auto format
 vim.g.autoformat = false
@@ -13,54 +13,54 @@ vim.g.autoformat = false
 -- * the name of a detector function like `lsp` or `cwd`
 -- * a pattern or array of patterns like `.git` or `lua`.
 -- * a function with signature `function(buf) -> string|string[]`
-vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+vim.g.root_spec = { 'lsp', { '.git', 'lua' }, 'cwd' }
 
-LazyVim.terminal.setup "zsh"
+LazyVim.terminal.setup 'zsh'
 local opt = vim.opt
 
 opt.autowrite = true -- Enable auto write
 -- opt.clipboard = "unnamedplus" -- Sync with system clipboard
-opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+opt.clipboard = vim.env.SSH_TTY and '' or 'unnamedplus' -- Sync with system clipboard
 opt.conceallevel = 0 -- Hide/show * markup for bold and italic
 -- opt.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience
-opt.completeopt = "menu,menuone,noselect"
+opt.completeopt = 'menu,menuone,noselect'
 opt.backup = false -- create a backup file
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
 opt.expandtab = true -- Use spaces instead of tabs
-opt.formatoptions = "jcroqlnt" -- tcqj
-opt.grepformat = "%f:%l:%c:%m"
-opt.grepprg = "rg --vimgrep"
+opt.formatoptions = 'jcroqlnt' -- tcqj
+opt.grepformat = '%f:%l:%c:%m'
+opt.grepprg = 'rg --vimgrep'
 opt.ignorecase = true -- Ignore case
-opt.inccommand = "nosplit" -- preview incremental substitute
+opt.inccommand = 'nosplit' -- preview incremental substitute
 opt.laststatus = 3 -- global statusline
 -- opt.listchars = { eol = "↵" }   -- End of line characters
 opt.listchars = {
-  eol = "⤶",
+  eol = '⤶',
   -- space = "❤",
   -- trail = "✚",
-  extends = "◀",
-  precedes = "▶",
+  extends = '◀',
+  precedes = '▶',
 }
 opt.list = true -- Show some invisible characters (tabs...
-opt.mouse = "a" -- Enable mouse mode
+opt.mouse = 'a' -- Enable mouse mode
 opt.number = true -- Print line number
 opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
 opt.scrolloff = 4 -- Lines of context
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" }
+opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp' }
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append { W = true, I = true, c = true, C = true }
 opt.showmode = false -- Dont show mode since we have a statusline
 opt.sidescrolloff = 8 -- Columns of context
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.signcolumn = 'yes' -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
-opt.spelllang = { "en" }
+opt.spelllang = { 'en' }
 opt.splitbelow = true -- Put new windows below current
-opt.splitkeep = "screen"
+opt.splitkeep = 'screen'
 opt.splitright = true -- Put new windows right of current
 opt.tabstop = 2 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
@@ -68,21 +68,21 @@ opt.timeoutlen = 300
 opt.undofile = true
 opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
-opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.virtualedit = 'block' -- Allow cursor to move where there is no text in visual block mode
+opt.wildmode = 'longest:full,full' -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = true -- Enable line wrap
 opt.fillchars = {
-  foldopen = "",
-  foldclose = "",
+  foldopen = '',
+  foldclose = '',
   -- fold = "⸱",
-  fold = " ",
-  foldsep = " ",
-  diff = "╱",
-  eob = " ",
+  fold = ' ',
+  foldsep = ' ',
+  diff = '╱',
+  eob = ' ',
 }
 
-if vim.fn.has "nvim-0.10" == 1 then
+if vim.fn.has 'nvim-0.10' == 1 then
   opt.smoothscroll = true
 end
 
@@ -90,22 +90,22 @@ end
 vim.opt.foldlevel = 99
 vim.opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
 
-if vim.fn.has "nvim-0.10" == 1 then
+if vim.fn.has 'nvim-0.10' == 1 then
   opt.smoothscroll = true
   opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
-  opt.foldmethod = "expr"
-  opt.foldtext = ""
+  opt.foldmethod = 'expr'
+  opt.foldtext = ''
 else
-  opt.foldmethod = "indent"
+  opt.foldmethod = 'indent'
   opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
 end
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
 
 -- set the global variable NOTE: https://www.lazyvim.org/extras/lang/python
-vim.g.python_host_prog = "/usr/sbin/python3"
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_ruff = "ruff_lsp"
+vim.g.python_host_prog = '/usr/sbin/python3'
+vim.g.lazyvim_python_lsp = 'pyright'
+vim.g.lazyvim_python_ruff = 'ruff_lsp'
 
 -- get the global variable
 -- local python_path = vim.g.python_host_prog
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,23 +1,23 @@
 return {
   {
-    "lewis6991/gitsigns.nvim",
+    'lewis6991/gitsigns.nvim',
     lazy = false,
     config = function()
       ---@diagnostic disable-next-line: unused-local
-      local icons = require "config.icons"
-      require("gitsigns").setup {
+      local icons = require 'config.icons'
+      require('gitsigns').setup {
         signs = {
-          add = { text = "+" },
+          add = { text = '+' },
           -- add = {
           --   hl = "GitSignsAdd",
           --   text = icons.git.LineAdded ,
           --   numhl = "GitSignsAddNr",
           --   linehl = "GitSignsAddLn",
           -- },
-          change = { text = "~" },
-          delete = { text = "_" },
-          topdelete = { text = "‾" },
-          changedelete = { text = "~" },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
         },
         signcolumn = true,
         numhl = false,
@@ -31,19 +31,19 @@ return {
         current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
           virt_text = true,
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
           delay = 1000,
           ignore_whitespace = false,
         },
-        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
         sign_priority = 6,
         status_formatter = nil,
         update_debounce = 200,
         max_file_length = 40000,
         preview_config = {
-          border = "rounded",
-          style = "minimal",
-          relative = "cursor",
+          border = 'rounded',
+          style = 'minimal',
+          relative = 'cursor',
           row = 0,
           col = 1,
         },
@@ -51,39 +51,39 @@ return {
 
         on_attach = function(bufnr)
           vim.keymap.set(
-            "n",
-            "<leader>gH",
-            require("gitsigns").preview_hunk,
-            { buffer = bufnr, desc = "Preview git hunk" }
+            'n',
+            '<leader>gH',
+            require('gitsigns').preview_hunk,
+            { buffer = bufnr, desc = 'Preview git hunk' }
           )
 
           vim.keymap.set(
-            "n",
-            "<leader>gR",
-            require("gitsigns").reset_hunk,
-            { buffer = bufnr, desc = "Reset git hunk (line)" }
+            'n',
+            '<leader>gR',
+            require('gitsigns').reset_hunk,
+            { buffer = bufnr, desc = 'Reset git hunk (line)' }
           )
-          vim.keymap.set("n", "]]", require("gitsigns").next_hunk, { buffer = bufnr, desc = "Next git hunk" })
-          vim.keymap.set("n", "[[", require("gitsigns").prev_hunk, { buffer = bufnr, desc = "Previous git hunk" })
+          vim.keymap.set('n', ']]', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Next git hunk' })
+          vim.keymap.set('n', '[[', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Previous git hunk' })
         end,
       }
     end,
   },
   {
-    "NeogitOrg/neogit",
+    'NeogitOrg/neogit',
     dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
 
       -- Only one of these is needed, not both.
-      "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
     },
     config = true,
     opts = {
-      kind = "tab",
+      kind = 'auto',
       commit_editor = {
-        kind = "auto",
+        kind = 'replace',
         show_staged_diff = true,
         -- Accepted values:
         -- "split" to show the staged diff below the commit editor
@@ -91,18 +91,18 @@ return {
         -- "split_above" Like :top split
         -- "vsplit_left" like :vsplit, but open to the left
         -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
-        staged_diff_split_kind = "split",
+        staged_diff_split_kind = 'split',
       },
-      commit_select_view = {
-        kind = "tab",
-      },
+      -- commit_select_view = {
+      -- kind = 'tab',
+      -- },
       commit_view = {
-        kind = "vsplit",
-        verify_commit = vim.fn.executable "gpg" == 1, -- Can be set to true or false, otherwise we try to find the binary
+        kind = 'auto',
+        verify_commit = vim.fn.executable 'gpg' == 1, -- Can be set to true or false, otherwise we try to find the binary
       },
     },
     keys = {
-      { "<leader>gg", "<cmd>Neogit kind=vsplit <cr>", desc = "Open Meogit" },
+      { '<leader>gg', '<cmd>Neogit kind=vsplit <cr>', desc = 'Open Meogit' },
       -- { "<F4>", ":DiffviewClose <cr>", desc = "Close Diff View" }, -- closing Diffview
     },
   },
@@ -120,17 +120,17 @@ return {
   -- },
 
   {
-    "linrongbin16/gitlinker.nvim",
-    event = "BufRead",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+    'linrongbin16/gitlinker.nvim',
+    event = 'BufRead',
+    dependencies = { { 'nvim-lua/plenary.nvim' } },
     config = function()
-      local wk = require "which-key"
+      local wk = require 'which-key'
       wk.register {
-        ["<leader>gy"] = { "<cmd>GitLink<cr>", "Git link" },
-        ["<leader>gY"] = { "<cmd>GitLink blame<cr>", "Git link blame" },
+        ['<leader>gy'] = { '<cmd>GitLink<cr>', 'Git link' },
+        ['<leader>gY'] = { '<cmd>GitLink blame<cr>', 'Git link blame' },
       }
 
-      require("gitlinker").setup {
+      require('gitlinker').setup {
         message = true,
         -- mapping = {
         --   ["<leader>gY"] = {
@@ -148,13 +148,13 @@ return {
         -- remote = 'github', -- force the use of a specific remote
         router = {
           browse = {
-            ["^git.gtech.com"] = "https://git.gtech.com/projects/"
-              .. "{_A.ORG}/"
-              .. "repos/{_A.REPO}/"
+            ['^git.gtech.com'] = 'https://git.gtech.com/projects/'
+              .. '{_A.ORG}/'
+              .. 'repos/{_A.REPO}/'
               -- .. "{_A.REV}/"
-              .. "browse/{_A.FILE}"
-              .. "?at=refs/heads/{_A.CURRENT_BRANCH}"
-              .. "#{_A.LSTART}"
+              .. 'browse/{_A.FILE}'
+              .. '?at=refs/heads/{_A.CURRENT_BRANCH}'
+              .. '#{_A.LSTART}'
               -- .. "{_A.LEND > _A.LSTART and ('&lines-count=' .. _A.LEND - _A.LSTART + 1) or ''}",
               .. "{(_A.LEND > _A.LSTART and (':' .. _A.LEND) or '')}",
           },
@@ -163,29 +163,29 @@ return {
     end,
   },
   {
-    "sindrets/diffview.nvim",
-    event = "BufRead",
+    'sindrets/diffview.nvim',
+    event = 'BufRead',
     dependencies = {
-      "nvim-lua/plenary.nvim",
+      'nvim-lua/plenary.nvim',
     },
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
     keys = {
-      { "<leader>gD", "<cmd>DiffviewOpen <cr>", desc = "Open DiffviewOpen" },
+      { '<leader>gD', '<cmd>DiffviewOpen <cr>', desc = 'Open DiffviewOpen' },
       -- ["<F4>"] = { ":DiffviewClose<cr>", desc = "Close Diff View" }, -- closing Diffview
-      { "<F4>", ":DiffviewClose <cr>", desc = "Close Diff View" }, -- closing Diffview
+      { '<F4>', ':DiffviewClose <cr>', desc = 'Close Diff View' }, -- closing Diffview
     },
   },
 
-  "petertriho/cmp-git",
+  'petertriho/cmp-git',
 
   config = function()
-    local format = require "cmp_git.format"
-    local sort = require "cmp_git.sort"
+    local format = require 'cmp_git.format'
+    local sort = require 'cmp_git.sort'
 
-    require("cmp_git").setup {
+    require('cmp_git').setup {
       -- defaults
-      filetypes = { "gitcommit", "octo", "NeogitCommitMessage" },
-      remotes = { "upstream", "origin" }, -- in order of most to least prioritized
+      filetypes = { 'gitcommit', 'octo', 'NeogitCommitMessage' },
+      remotes = { 'upstream', 'origin' }, -- in order of most to least prioritized
       enableRemoteUrlRewrites = false, -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
       git = {
         commits = {
@@ -197,10 +197,10 @@ return {
       github = {
         hosts = {}, -- list of private instances of github
         issues = {
-          fields = { "title", "number", "body", "updatedAt", "state" },
-          filter = "all", -- assigned, created, mentioned, subscribed, all, repos
+          fields = { 'title', 'number', 'body', 'updatedAt', 'state' },
+          filter = 'all', -- assigned, created, mentioned, subscribed, all, repos
           limit = 100,
-          state = "open", -- open, closed, all
+          state = 'open', -- open, closed, all
           sort_by = sort.github.issues,
           format = format.github.issues,
         },
@@ -210,9 +210,9 @@ return {
           format = format.github.mentions,
         },
         pull_requests = {
-          fields = { "title", "number", "body", "updatedAt", "state" },
+          fields = { 'title', 'number', 'body', 'updatedAt', 'state' },
           limit = 100,
-          state = "open", -- open, closed, merged, all
+          state = 'open', -- open, closed, merged, all
           sort_by = sort.github.pull_requests,
           format = format.github.pull_requests,
         },
@@ -221,7 +221,7 @@ return {
         hosts = {}, -- list of private instances of gitlab
         issues = {
           limit = 100,
-          state = "opened", -- opened, closed, all
+          state = 'opened', -- opened, closed, all
           sort_by = sort.gitlab.issues,
           format = format.gitlab.issues,
         },
@@ -232,50 +232,50 @@ return {
         },
         merge_requests = {
           limit = 100,
-          state = "opened", -- opened, closed, locked, merged
+          state = 'opened', -- opened, closed, locked, merged
           sort_by = sort.gitlab.merge_requests,
           format = format.gitlab.merge_requests,
         },
       },
       trigger_actions = {
         {
-          debug_name = "git_commits",
-          trigger_character = ":",
+          debug_name = 'git_commits',
+          trigger_character = ':',
           action = function(sources, trigger_char, callback, params, git_info)
             return sources.git:get_commits(callback, params, trigger_char)
           end,
         },
         {
-          debug_name = "gitlab_issues",
-          trigger_character = "#",
+          debug_name = 'gitlab_issues',
+          trigger_character = '#',
           action = function(sources, trigger_char, callback, params, git_info)
             return sources.gitlab:get_issues(callback, git_info, trigger_char)
           end,
         },
         {
-          debug_name = "gitlab_mentions",
-          trigger_character = "@",
+          debug_name = 'gitlab_mentions',
+          trigger_character = '@',
           action = function(sources, trigger_char, callback, params, git_info)
             return sources.gitlab:get_mentions(callback, git_info, trigger_char)
           end,
         },
         {
-          debug_name = "gitlab_mrs",
-          trigger_character = "!",
+          debug_name = 'gitlab_mrs',
+          trigger_character = '!',
           action = function(sources, trigger_char, callback, params, git_info)
             return sources.gitlab:get_merge_requests(callback, git_info, trigger_char)
           end,
         },
         {
-          debug_name = "github_issues_and_pr",
-          trigger_character = "#",
+          debug_name = 'github_issues_and_pr',
+          trigger_character = '#',
           action = function(sources, trigger_char, callback, params, git_info)
             return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
           end,
         },
         {
-          debug_name = "github_mentions",
-          trigger_character = "@",
+          debug_name = 'github_mentions',
+          trigger_character = '@',
           action = function(sources, trigger_char, callback, params, git_info)
             return sources.github:get_mentions(callback, git_info, trigger_char)
           end,
