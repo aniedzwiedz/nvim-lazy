849f70857e44e1530a2c32363dbc6f8ec9548d11
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2024-06-28 00:11:20 +0200

    hover

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
-local Util = require "lazyvim.util"
+local Util = require 'lazyvim.util'
 -- Silent keymap option
 -- local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 -- yank to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank all' })
 
 -- map("n", "<leader>?", function()
 --   -- You can pass additional configuration to telescope to change theme, layout, etc.
@@ -22,47 +22,47 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 -- end, { desc = "[?] Fuzzily search in current buffer]" })
 
 -- Add toggle gitsigns blame line
-if Util.has "gitsigns.nvim" then
+if Util.has 'gitsigns.nvim' then
   map(
-    "n",
-    "<leader>ub",
+    'n',
+    '<leader>ub',
     "<cmd>lua require('gitsigns').toggle_current_line_blame()<CR>",
-    { desc = "Toggle current line blame" }
+    { desc = 'Toggle current line blame' }
   )
-  map("n", "<leader>gl", function()
-    require("gitsigns").blame_line { full = false }
-  end, { desc = "View full Blame" })
-  map("n", "<leader>gL", function()
-    require("gitsigns").blame_line { full = true }
-  end, { desc = "View full Git Blame" })
+  map('n', '<leader>gl', function()
+    require('gitsigns').blame_line { full = false }
+  end, { desc = 'View full Blame' })
+  map('n', '<leader>gL', function()
+    require('gitsigns').blame_line { full = true }
+  end, { desc = 'View full Git Blame' })
   -- map("n", "<leader>gdo", ":DiffviewOpen<cr>", { desc = "DiffviewOpen " })
 end
 
 -- Select all
-map("n", "<leader>a", "ggVG", { desc = "Select all" })
+map('n', '<leader>a', 'ggVG', { desc = 'Select all' })
 
 -- Change Commit Log to use Lazygit
-map("n", "<leader>gd", function()
-  LazyVim.lazygit { args = { "log" } }
-end, { desc = "Lazygit Commit Log" })
+map('n', '<leader>gd', function()
+  LazyVim.lazygit { args = { 'log' } }
+end, { desc = 'Lazygit Commit Log' })
 
 -- Copy file paths
-map("n", "<leader>fz", '<cmd>let @+ = expand("%")<CR>', { desc = "Copy File Name" })
-map("n", "<leader>fZ", '<cmd>let @+ = expand("%:p")<CR>', { desc = "Copy File Path" })
+map('n', '<leader>fz', '<cmd>let @+ = expand("%")<CR>', { desc = 'Copy File Name' })
+map('n', '<leader>fZ', '<cmd>let @+ = expand("%:p")<CR>', { desc = 'Copy File Path' })
 
 -- Replace word under cursor across entire buffer
 map(
-  "n",
+  'n',
   -- "<leader>cw",
-  "<F2>",
+  '<F2>',
   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-  { desc = "[c]hange word under cursor" }
+  { desc = '[c]hange word under cursor' }
 )
 
 -- Filetype-specific keymaps (these can be done in the ftplugin directory instead if you prefer)
-map("n", "<leader>go", function()
-  if vim.bo.filetype == "python" then
-    vim.api.nvim_command "PyrightOrganizeImports"
+map('n', '<leader>go', function()
+  if vim.bo.filetype == 'python' then
+    vim.api.nvim_command 'PyrightOrganizeImports'
   end
 end)
 
